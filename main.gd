d727da14ecaa881037d01a7ef0c8226c5d77e36d
Spagheet <jakewills57@gmail.com>
2023-10-20 08:35:12 +0200

    stuff

--- main.gd
+++ main.gd
@@ -1,20 +1,23 @@
 extends Node3D
 enum State {LOBBY, RUNNING, END}
-var state:State
+@onready var state:State
+@onready var start = $HUD/StartPanel
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	state = State.LOBBY
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	match(state) :
-		State.LOBBY : {
+	var enemy_array = []
+	match state :
+		State.LOBBY :
 			# LOBBY STATE
-			
-		}
-		State.RUNNING : {
+			if Input.is_action_just_pressed("ui_accept"):
+				state = State.RUNNING
+				start.visible = false
+		State.RUNNING : 
 			# RUNNING STATE
-		}
-		State.END : {
-			# END STATE
-		}
+			pass
+		State.END : 
+			start.visible = true
+			pass
